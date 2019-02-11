9ea8279d8fc6d535d8f99d5856dbea834c8270fc
unknown <dusan.plasienka@gmail.com>
2019-02-11 19:09:56 +0100

    First and last commit on Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## USAGE example -----------------------------------------------------------
 
-## Write a short comment describing this function
+# m <- makeCacheMatrix(matrix(rnorm(25),5,5)) # generate the special matrix object
+# m$get() # print out the matrix object values ()
+# m_inv <- cacheSolve(m)
+# m_inv_test <- solve(m$get()) # the same inverse should this be
+# identical(m_inv, m_inv_test) # this I expected would still be false due to
+# rounding errors in floating points but it seems to work:)
 
-makeCacheMatrix <- function(x = matrix()) {
+## ------------------------------------------------------------------------
 
+## function creates a "matrix class" that contains 4 functions to manipulate
+## the matrix object with (set value, get value, set inverse, get inverse)
+makeCacheMatrix <- function(m = matrix()) {
+
+     inv <- NULL
+     set <- function(new_m) {
+         m <<- new_m
+         inv <<- NULL
+     }
+     get <- function() m
+     setinverse <- function(inverse) inv <<- inverse
+     getinverse <- function() inv
+     list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
+## function that calculates the inverse of the special "matrix class" created
+## by the makeCacheMatrix() function; it first checks if the inverse has already
+## been calculated and if yes, it loads the inverse matrix from cache. If not,
+## it calculates the inverse by the solve() function and sets it to the matrix object
+cacheSolve <- function(m, ...) {
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- m$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached inverse matrix")
+        return(inv)
+    }
+    m_data <- m$get()
+    inv <- solve(m_data, ...)
+    m$setinverse(inv)
+    inv # return inverse
 }
